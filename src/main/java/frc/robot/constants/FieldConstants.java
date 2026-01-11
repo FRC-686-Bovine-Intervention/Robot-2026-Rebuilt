a8ab666ebc6c49666f18cea7f11ebc9970fdc888
Michael Sims <michael.sims@bovineintervention.org>
2026-01-11 11:48:02 -0500

    WIP on fieldconstants

--- src/main/java/frc/robot/constants/FieldConstants.java
+++ src/main/java/frc/robot/constants/FieldConstants.java
@@ -5,13 +5,15 @@ import static edu.wpi.first.units.Units.Meters;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.units.measure.Distance;
 import frc.util.flipping.AllianceFlipped;
 import frc.util.geometry.PoseBoundingBoxUtil.VerticalLine;
 
 public final class FieldConstants {
-	public static final Distance fieldLength = Inches.of(648.000000);
-	public static final Distance fieldWidth =  Inches.of(324.00000);
+	// From AndyMark field CAD, wall base to wall base
+	public static final Distance fieldLength = Inches.of(650.118760);
+	public static final Distance fieldWidth =  Inches.of(316.641000);
 
 	public static final AllianceFlipped<VerticalLine> allianceBox = AllianceFlipped.fromBlue(new VerticalLine(fieldLength.div(2.0).in(Meters), false));
 
@@ -25,4 +27,21 @@ public final class FieldConstants {
 		}
 		apriltagLayout = a;
 	}
+
+	public static final AllianceFlipped<Distance> robotStartingLineCenterX = AllianceFlipped.fromBlueXPos(Inches.of(157.059380));
+	public static final AllianceFlipped<Distance> barrierLineCenterX = AllianceFlipped.fromBlueXPos(Inches.of(189.816986));
+
+	public static final AllianceFlipped<Translation2d> hubCenter = AllianceFlipped.fromBlue(new Translation2d(
+		barrierLineCenterX.getBlue(),
+		Inches.of(158.320500)
+	));
+	public static final Distance hubHeight = Inches.of(72.000000);
+
+	public static final AllianceFlipped<Translation2d> towerRungsCenter = AllianceFlipped.fromBlue(new Translation2d(
+		Inches.of(42.047000),
+		Inches.of(146.858)
+	));
+	public static final Distance towerInnerUprightDistance = Inches.of(32.250000);
+
+	public static final AllianceFlipped<Distance> outpostCenterY = AllianceFlipped.fromBlueYPos(Inches.of(25.621000));
 }
