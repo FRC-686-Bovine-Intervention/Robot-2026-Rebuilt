4648e7efc5af6e8a036d68113d3275ed2592e16d
Michael Sims <michael.sims@bovineintervention.org>
2026-01-16 16:44:13 -0500

    minor robot constants refactor

--- src/main/java/frc/robot/constants/RobotConstants.java
+++ src/main/java/frc/robot/constants/RobotConstants.java
@@ -7,8 +7,6 @@ import static edu.wpi.first.units.Units.Meters;
 import static edu.wpi.first.units.Units.Pounds;
 import static edu.wpi.first.units.Units.Seconds;
 
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.units.measure.Frequency;
 import edu.wpi.first.units.measure.Mass;
@@ -20,7 +18,7 @@ public final class RobotConstants {
 	public static final boolean tuningMode = true;
 
 	public static final Mass robotWeight = Pounds.of(125);
-	public static final MomentOfInertia robotMOI = KilogramSquareMeters.of(2);
+	public static final MomentOfInertia robotMOI = KilogramSquareMeters.of(6);
 
 	public static final Distance frameLength = Inches.of(26);
 	public static final Distance frameWidth = Inches.of(26);
@@ -28,19 +26,17 @@ public final class RobotConstants {
 	public static final Distance centerToFrontFrame = frameLength.div(2);
 	public static final Distance centerToSideFrame = frameWidth.div(2);
 
-	public static final Distance bumperWidth = Inches.of(4);
+	public static final Distance bumperThickness = Inches.of(4);
 
-	public static final Distance centerToFrontBumper = centerToFrontFrame.plus(bumperWidth);
-	public static final Distance centerToSideBumper = centerToSideFrame.plus(bumperWidth);
+	public static final Distance centerToFrontBumper = centerToFrontFrame.plus(bumperThickness);
+	public static final Distance centerToSideBumper = centerToSideFrame.plus(bumperThickness);
 
 	/**Distance between back bumper and front bumper, aka in the X axis */
 	public static final Distance robotLength = centerToFrontBumper.times(2);
 	/**Distance between left bumper and right bumper, aka in the Y axis */
 	public static final Distance robotWidth = centerToSideBumper.times(2);
 
-	public static final Rotation2d intakeForward = Rotation2d.kZero;
-
-	public static final Distance centerToBumperCorner = Meters.of(new Translation2d(robotLength, robotWidth).getNorm());
+	public static final Distance centerToBumperCorner = Meters.of(Math.hypot(centerToFrontBumper.in(Meters), centerToSideBumper.in(Meters)));
 
 	public static final double rioUpdatePeriodSecs = Robot.defaultPeriodSecs;
 	public static final Time rioUpdatePeriod = Seconds.of(rioUpdatePeriodSecs);
