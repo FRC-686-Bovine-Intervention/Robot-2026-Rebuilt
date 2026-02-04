25e2f5ed75e9762fd68fc4ad2fdd50e97762672b
Ladle <77344665+The-Ladle@users.noreply.github.com>
2026-02-04 01:47:42 -0500

    Make github happy with spotless build

--- src/main/java/frc/robot/subsystems/vision/object/ObjectVision.java
+++ src/main/java/frc/robot/subsystems/vision/object/ObjectVision.java
@@ -28,25 +28,25 @@ public class ObjectVision {
 	private final ObjectPipeline[] pipelines;
 
 	private static final LoggedTunable<Distance> updateDistanceThreshold = LoggedTunable.from("Vision/Object/Updating/Update Distance Threshold", Meters::of, 0.1);
-    private static final LoggedTunableNumber posUpdatingFilteringFactor =  LoggedTunable.from("Vision/Object/Updating/Pos Updating Filtering Factor", 0.8);
-    //private static final LoggedTunableNumber confUpdatingFilteringFactor = LoggedTunable.from("Vision/Object/Confidence/Updating Filtering Factor", 0.5);
-    private static final LoggedTunableNumber confidenceDecayPerSecond =    LoggedTunable.from("Vision/Object/Confidence/Decay Per Second", 3);
-    private static final LoggedTunableNumber detargetConfidenceThreshold = LoggedTunable.from("Vision/Object/Target Threshold/Detarget", 0.5);
-    
-    private List<TrackedObject> trackedObjects = new ArrayList<>(0);
-    
-    public ObjectVision(ObjectPipeline... pipelines) {
-        System.out.println("[Init ObjectVision] Instantiating ObjectVision");
-        this.pipelines = pipelines;
-    }
+	private static final LoggedTunableNumber posUpdatingFilteringFactor =  LoggedTunable.from("Vision/Object/Updating/Pos Updating Filtering Factor", 0.8);
+	//private static final LoggedTunableNumber confUpdatingFilteringFactor = LoggedTunable.from("Vision/Object/Confidence/Updating Filtering Factor", 0.5);
+	private static final LoggedTunableNumber confidenceDecayPerSecond =    LoggedTunable.from("Vision/Object/Confidence/Decay Per Second", 3);
+	private static final LoggedTunableNumber detargetConfidenceThreshold = LoggedTunable.from("Vision/Object/Target Threshold/Detarget", 0.5);
 
-    public void periodic() {
-        LoggedTracer.logEpoch("CommandScheduler Periodic/ObjectVision/Before");
-        for (var pipeline : this.pipelines) {
-            var frames = pipeline.getFrames();
-            var loggingKey = "Vision/Objects/Results/" + pipeline.pipelineIndex + "/";
-            for (var frame : frames) {
-                List<TrackedObject> frameTargets = new ArrayList<>();
+	private List<TrackedObject> trackedObjects = new ArrayList<>(0);
+
+	public ObjectVision(ObjectPipeline... pipelines) {
+		System.out.println("[Init ObjectVision] Instantiating ObjectVision");
+		this.pipelines = pipelines;
+	}
+
+	public void periodic() {
+		LoggedTracer.logEpoch("CommandScheduler Periodic/ObjectVision/Before");
+		for (var pipeline : this.pipelines) {
+			var frames = pipeline.getFrames();
+			var loggingKey = "Vision/Objects/Results/" + pipeline.pipelineIndex + "/";
+			for (var frame : frames) {
+				List<TrackedObject> frameTargets = new ArrayList<>();
 				for (int i = 0; i < frame.targets.length; i++) {
 					var target = TrackedObject.from(pipeline.camera.mount, frame.targets[i]);
 					if (target.isPresent()) {
@@ -54,26 +54,26 @@ public class ObjectVision {
 					}
 				}
 
-                for (var trackedObject : trackedObjects) {
-                    trackedObject.resetUpdated();
-                }
-                // Update tracked objects
-                for (var frameTarget : frameTargets) {
-                    for (var trackedObject : trackedObjects) {
-                        trackedObject.decayConfidence(confidenceDecayPerSecond.getAsDouble());
+				for (var trackedObject : trackedObjects) {
+					trackedObject.resetUpdated();
+				}
+				// Update tracked objects
+				for (var frameTarget : frameTargets) {
+					for (var trackedObject : trackedObjects) {
+						trackedObject.decayConfidence(confidenceDecayPerSecond.getAsDouble());
 
-                        if (trackedObject.fieldPos.getDistance(frameTarget.fieldPos) < updateDistanceThreshold.get().in(Meters) && !trackedObject.updated) {
-                            trackedObject.updateWithFiltering(frameTarget);
-                        } else {
-                            trackedObjects.add(frameTarget);
-                        }
+						if (trackedObject.fieldPos.getDistance(frameTarget.fieldPos) < updateDistanceThreshold.get().in(Meters) && !trackedObject.updated) {
+							trackedObject.updateWithFiltering(frameTarget);
+						} else {
+							trackedObjects.add(frameTarget);
+						}
 
-                        if (trackedObject.confidence < detargetConfidenceThreshold.getAsDouble()) {
-                            trackedObjects.remove(trackedObject);
-                        }
-                    }
-                }
-                
+						if (trackedObject.confidence < detargetConfidenceThreshold.getAsDouble()) {
+							trackedObjects.remove(trackedObject);
+						}
+					}
+				}
+
 				List<List<Pose3d>> loggedTargets = new ArrayList<>();
 				for (int i = 0; i < ObjectVisionConstants.trackableObjects.length; i++) {
 					loggedTargets.add(new ArrayList<>());
@@ -84,146 +84,146 @@ public class ObjectVision {
 				for (int i = 0; i < ObjectVisionConstants.trackableObjects.length; i++) {
 					Logger.recordOutput(loggingKey + "Targets/" + ObjectVisionConstants.trackableObjects[i].className, (Pose3d[]) loggedTargets.get(i).toArray());
 				}
-            }
-        }
-    }
-    
-    public static class TrackedObject {
-        public TrackableObject type;
-        public Translation2d fieldPos;
-        public double confidence;
-        public boolean updated;
+			}
+		}
+	}
 
-        public TrackedObject(TrackableObject type, Translation2d fieldPos, double confidence) {
-            this.type = type;
-            this.fieldPos = fieldPos;
-            this.confidence = confidence;
-            this.updated = true;
-        }
+	public static class TrackedObject {
+		public TrackableObject type;
+		public Translation2d fieldPos;
+		public double confidence;
+		public boolean updated;
 
-        public static Optional<TrackedObject> from(CameraMount mount, CameraTarget target) {
-            var camRobotPose = mount.getRobotRelative();
-            var camFieldPose = mount.getFieldRelative();
-            
-            var tty = target.pitchRads + camRobotPose.getRotation().getY();
-            var ttx = -target.yawRads;
+		public TrackedObject(TrackableObject type, Translation2d fieldPos, double confidence) {
+			this.type = type;
+			this.fieldPos = fieldPos;
+			this.confidence = confidence;
+			this.updated = true;
+		}
+
+		public static Optional<TrackedObject> from(CameraMount mount, CameraTarget target) {
+			var camRobotPose = mount.getRobotRelative();
+			var camFieldPose = mount.getFieldRelative();
+
+			var tty = target.pitchRads + camRobotPose.getRotation().getY();
+			var ttx = -target.yawRads;
 
 			var type = ObjectVisionConstants.trackableObjects[target.objectClassID];
 
-            var height = type.height.in(Meters) / 2.0 - camRobotPose.getTranslation().getZ();
-            var h = height / Math.tan(tty);
-            var x = Math.cos(ttx) * h;
-            var y = Math.sin(ttx) * h;
+			var height = type.height.in(Meters) / 2.0 - camRobotPose.getTranslation().getZ();
+			var h = height / Math.tan(tty);
+			var x = Math.cos(ttx) * h;
+			var y = Math.sin(ttx) * h;
 
-            var objectFieldPose = camFieldPose.toPose2d().transformBy(new Transform2d(
-                new Translation2d(
-                    x,
-                    y
-                ),
-                Rotation2d.kZero
-            ));
-            
-            return Optional.of(new TrackedObject(type, objectFieldPose.getTranslation(), target.objectConfidence));
-        }
+			var objectFieldPose = camFieldPose.toPose2d().transformBy(new Transform2d(
+				new Translation2d(
+					x,
+					y
+				),
+				Rotation2d.kZero
+			));
 
-        // public void updateConfidence() {
-        //     confidence += confidence * MathUtil.clamp(1 - confUpdatingFilteringFactor.get(), 0, 1); 
-        // }
+			return Optional.of(new TrackedObject(type, objectFieldPose.getTranslation(), target.objectConfidence));
+		}
 
-        public void updateWithFiltering(TrackedObject newObject) {
-            this.fieldPos = fieldPos.interpolate(newObject.fieldPos, posUpdatingFilteringFactor.get());
-            this.confidence = newObject.confidence;
-            this.updated = true;
-        }
+		// public void updateConfidence() {
+		//     confidence += confidence * MathUtil.clamp(1 - confUpdatingFilteringFactor.get(), 0, 1);
+		// }
 
-        public void resetUpdated() {
-            this.updated = false;
-        }
+		public void updateWithFiltering(TrackedObject newObject) {
+			this.fieldPos = fieldPos.interpolate(newObject.fieldPos, posUpdatingFilteringFactor.get());
+			this.confidence = newObject.confidence;
+			this.updated = true;
+		}
 
-        public void decayConfidence(double rate) {
-            this.confidence -= confidenceDecayPerSecond.get() * rate * RobotConstants.rioUpdatePeriodSecs;
-        }
+		public void resetUpdated() {
+			this.updated = false;
+		}
 
-        // public double getPriority(ChassisSpeeds desiredRobotRelativeSpeeds) {
-        //     // var pose = RobotState.getInstance().getEstimatedGlobalPose();
-        //     // var FORR = fieldPos.minus(pose.getTranslation());
-        //     // var rotation = pose.getRotation().minus(RobotConstants.intakeForward);
-        //     // return 
-        //     //     confidence * priorityPerConfidence.get() *
-        //     //     VecBuilder.fill(rotation.getCos(), rotation.getSin()).dot(FORR.toVector().unit()) + 
-        //     //     FORR.getNorm() * priorityPerDistance.get()
-        //     // ;
-        //     var intakePose = RobotState.getInstance().getEstimatedGlobalPose(); //TODO: Replace with a mount where the intake would be
-        //     var rel = new Pose2d(this.fieldPos, Rotation2d.kZero).relativeTo(intakePose);
+		public void decayConfidence(double rate) {
+			this.confidence -= confidenceDecayPerSecond.get() * rate * RobotConstants.rioUpdatePeriodSecs;
+		}
 
-        //     double dist = Math.hypot(rel.getX(), 2 * rel.getY());
-        //     double angle = Math.atan2(rel.getY(), rel.getX());
+		// public double getPriority(ChassisSpeeds desiredRobotRelativeSpeeds) {
+		//     // var pose = RobotState.getInstance().getEstimatedGlobalPose();
+		//     // var FORR = fieldPos.minus(pose.getTranslation());
+		//     // var rotation = pose.getRotation().minus(RobotConstants.intakeForward);
+		//     // return
+		//     //     confidence * priorityPerConfidence.get() *
+		//     //     VecBuilder.fill(rotation.getCos(), rotation.getSin()).dot(FORR.toVector().unit()) +
+		//     //     FORR.getNorm() * priorityPerDistance.get()
+		//     // ;
+		//     var intakePose = RobotState.getInstance().getEstimatedGlobalPose(); //TODO: Replace with a mount where the intake would be
+		//     var rel = new Pose2d(this.fieldPos, Rotation2d.kZero).relativeTo(intakePose);
 
-        //     double score = 0;
+		//     double dist = Math.hypot(rel.getX(), 2 * rel.getY());
+		//     double angle = Math.atan2(rel.getY(), rel.getX());
 
-        //     var chassisSpeedsAngle = Math.atan2(desiredRobotRelativeSpeeds.vxMetersPerSecond, desiredRobotRelativeSpeeds.vyMetersPerSecond);
-        //     if (Math.abs(angle - chassisSpeedsAngle) < Math.PI / 2 && (angle > Math.PI/2 || angle < -Math.PI/2)) {
-        //         score = (1 / (dist * priorityPerDistance.getAsDouble())) * (this.confidence * priorityPerConfidence.getAsDouble());
-        //     }
+		//     double score = 0;
 
-        //     return score;
-        // }
+		//     var chassisSpeedsAngle = Math.atan2(desiredRobotRelativeSpeeds.vxMetersPerSecond, desiredRobotRelativeSpeeds.vyMetersPerSecond);
+		//     if (Math.abs(angle - chassisSpeedsAngle) < Math.PI / 2 && (angle > Math.PI/2 || angle < -Math.PI/2)) {
+		//         score = (1 / (dist * priorityPerDistance.getAsDouble())) * (this.confidence * priorityPerConfidence.getAsDouble());
+		//     }
 
-        public Pose3d toASPose() {
-            return new Pose3d(
-                new Translation3d(
-                    fieldPos.getMeasureX(),
-                    fieldPos.getMeasureY(),
-                    type.height.div(2)
-                ),
-                new Rotation3d(
-                    Degrees.of(180),
-                    Degrees.zero(),
-                    Degrees.zero()
-                )
-            );
-        }
+		//     return score;
+		// }
 
-        // public static final TrackedObjectStruct struct = new TrackedObjectStruct();
-        // public static class TrackedObjectStruct implements Struct<TrackedObject> {
-        //     @Override
-        //     public Class<TrackedObject> getTypeClass() {
-        //         return TrackedObject.class;
-        //     }
+		public Pose3d toASPose() {
+			return new Pose3d(
+				new Translation3d(
+					fieldPos.getMeasureX(),
+					fieldPos.getMeasureY(),
+					type.height.div(2)
+				),
+				new Rotation3d(
+					Degrees.of(180),
+					Degrees.zero(),
+					Degrees.zero()
+				)
+			);
+		}
 
-        //     @Override
-        //     public String getTypeName() {
-        //         return "TrackedNote";
-        //     }
+		// public static final TrackedObjectStruct struct = new TrackedObjectStruct();
+		// public static class TrackedObjectStruct implements Struct<TrackedObject> {
+		//     @Override
+		//     public Class<TrackedObject> getTypeClass() {
+		//         return TrackedObject.class;
+		//     }
 
-        //     @Override
-        //     public int getSize() {
-        //         return Translation2d.struct.getSize() * 1 + kSizeDouble * 1;
-        //     }
+		//     @Override
+		//     public String getTypeName() {
+		//         return "TrackedNote";
+		//     }
 
-        //     @Override
-        //     public String getSchema() {
-        //         return "Translation2d fieldPos;double confidence";
-        //     }
+		//     @Override
+		//     public int getSize() {
+		//         return Translation2d.struct.getSize() * 1 + kSizeDouble * 1;
+		//     }
 
-        //     @Override
-        //     public Struct<?>[] getNested() {
-        //         return new Struct<?>[] {Translation2d.struct};
-        //     }
+		//     @Override
+		//     public String getSchema() {
+		//         return "Translation2d fieldPos;double confidence";
+		//     }
 
-        //     @Override
-        //     public TrackedObject unpack(ByteBuffer bb) {
-        //         var type = bb.getInt();
-        //         var fieldPos = Translation2d.struct.unpack(bb);
-        //         var confidence = bb.getDouble();
-        //         return new TrackedObject(type, fieldPos, confidence);
-        //     }
+		//     @Override
+		//     public Struct<?>[] getNested() {
+		//         return new Struct<?>[] {Translation2d.struct};
+		//     }
 
-        //     @Override
-        //     public void pack(ByteBuffer bb, TrackedObject value) {
-        //         Translation2d.struct.pack(bb, value.fieldPos);
-        //         bb.putDouble(value.confidence);
-        //     }
-        // }
-    }
+		//     @Override
+		//     public TrackedObject unpack(ByteBuffer bb) {
+		//         var type = bb.getInt();
+		//         var fieldPos = Translation2d.struct.unpack(bb);
+		//         var confidence = bb.getDouble();
+		//         return new TrackedObject(type, fieldPos, confidence);
+		//     }
+
+		//     @Override
+		//     public void pack(ByteBuffer bb, TrackedObject value) {
+		//         Translation2d.struct.pack(bb, value.fieldPos);
+		//         bb.putDouble(value.confidence);
+		//     }
+		// }
+	}
 }
--- src/main/java/frc/robot/subsystems/vision/object/ObjectVisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/object/ObjectVisionConstants.java
@@ -4,19 +4,19 @@ import edu.wpi.first.units.measure.Distance;
 import frc.robot.constants.FieldConstants;
 
 public class ObjectVisionConstants {
-    public static class TrackableObject {
-        public final Distance height;
-        public final int classId;
-        public final String className;
+	public static class TrackableObject {
+		public final Distance height;
+		public final int classId;
+		public final String className;
 
-        private TrackableObject(Distance height, int classId, String className) {
-            this.height = height;
-            this.classId = classId;
-            this.className = className;
-        }
-    }
+		private TrackableObject(Distance height, int classId, String className) {
+			this.height = height;
+			this.classId = classId;
+			this.className = className;
+		}
+	}
 
-    public static final TrackableObject[] trackableObjects = {
-        new TrackableObject(FieldConstants.fuelHeight, 0, "Fuel")
-    };
+	public static final TrackableObject[] trackableObjects = {
+		new TrackableObject(FieldConstants.fuelHeight, 0, "Fuel")
+	};
 }
