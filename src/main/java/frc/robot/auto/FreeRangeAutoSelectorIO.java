5a81571e289d8f84a3008c6bbc9f9be06e442cee
Ladle <77344665+The-Ladle@users.noreply.github.com>
2026-02-04 01:52:24 -0500

    Make github happy again

--- src/main/java/frc/robot/auto/FreeRangeAutoSelectorIO.java
+++ src/main/java/frc/robot/auto/FreeRangeAutoSelectorIO.java
@@ -3,14 +3,14 @@ package frc.robot.auto;
 import org.littletonrobotics.junction.AutoLog;
 
 public interface FreeRangeAutoSelectorIO {
-    @AutoLog
-    public static class FreeRangeAutoSelectorIOInputs {
-        public boolean shouldKeepOut = true;
-        public boolean shouldClimb = true;
+	@AutoLog
+	public static class FreeRangeAutoSelectorIOInputs {
+		public boolean shouldKeepOut = true;
+		public boolean shouldClimb = true;
 
-        public double[] keepOutTopLeft = new double[0];
-        public double[] keepOutTopRight = new double[0];
-    }
+		public double[] keepOutTopLeft = new double[0];
+		public double[] keepOutTopRight = new double[0];
+	}
 
-    public default void updateInputs(FreeRangeAutoSelectorIOInputs inputs) {}
+	public default void updateInputs(FreeRangeAutoSelectorIOInputs inputs) {}
 }
