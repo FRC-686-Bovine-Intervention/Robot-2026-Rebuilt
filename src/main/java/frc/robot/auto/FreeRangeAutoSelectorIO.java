ba9ed8fcc3301bf36b2f768c2cecf3b0963d5024
Ladle <77344665+The-Ladle@users.noreply.github.com>
2026-02-07 11:12:36 -0500

    Begin actual auto work
    May switch to command compositions with custom triggers

--- src/main/java/frc/robot/auto/FreeRangeAutoSelectorIO.java
+++ src/main/java/frc/robot/auto/FreeRangeAutoSelectorIO.java
@@ -7,9 +7,13 @@ public interface FreeRangeAutoSelectorIO {
 	public static class FreeRangeAutoSelectorIOInputs {
 		public boolean shouldKeepOut = true;
 		public boolean shouldClimb = true;
+		public boolean shouldNeutralZone = false;
+		public boolean shouldAvoidBump = false;
+		public boolean shouldNotAutoIntake = false;
+		public boolean shouldStopToShoot = false;
 
 		public double[] keepOutTopLeft = new double[0];
-		public double[] keepOutTopRight = new double[0];
+		public double[] keepOutBottomRight = new double[0];
 	}
 
 	public default void updateInputs(FreeRangeAutoSelectorIOInputs inputs) {}
--- src/main/java/frc/robot/auto/FreeRangeAutoSelectorIOServer.java
+++ src/main/java/frc/robot/auto/FreeRangeAutoSelectorIOServer.java
@@ -3,6 +3,7 @@ package frc.robot.auto;
 import edu.wpi.first.net.WebServer;
 import edu.wpi.first.networktables.BooleanSubscriber;
 import edu.wpi.first.networktables.DoubleArraySubscriber;
+import edu.wpi.first.networktables.DoubleSubscriber;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.PubSubOption;
 import edu.wpi.first.wpilibj.Filesystem;
@@ -15,12 +16,21 @@ public class FreeRangeAutoSelectorIOServer implements FreeRangeAutoSelectorIO {
 	private static final String keepOutBottomRightName = "KeepOut/BottomRight";
 
 	private static final String shouldClimbName = "ShouldClimb";
+	private static final String shouldNeutralZoneName = "ShouldNeutralZone";
+	private static final String shouldAvoidBumpName = "ShouldAvoidBump";
+	private static final String shouldNotAutoIntakeName = "ShouldNotAutoIntake";
+	private static final String shouldStopToShootName = "ShouldStopToShoot";
 
 	private final BooleanSubscriber keepOutSubscriber;
-	private final DoubleArraySubscriber topLeftSubscriber;
-	private final DoubleArraySubscriber bottomRightSubscriber;
-
+	private final DoubleSubscriber topLeftXSubscriber;
+	private final DoubleSubscriber topLeftYSubscriber;
+	private final DoubleSubscriber bottomRightXSubscriber;
+	private final DoubleSubscriber bottomRightYSubscriber;
 	private final BooleanSubscriber climbSubscriber;
+	private final BooleanSubscriber neutralZoneSubscriber;
+	private final BooleanSubscriber avoidBumpSubscriber;
+	private final BooleanSubscriber notAutoIntakeSubscriber;
+	private final BooleanSubscriber stopToShootSubscriber;
 
 	public FreeRangeAutoSelectorIOServer() {
 		System.out.println("[Init] Creating FreeRangeAutoSelectorIOServer");
@@ -33,14 +43,38 @@ public class FreeRangeAutoSelectorIOServer implements FreeRangeAutoSelectorIO {
 			inputTable
 			.getBooleanTopic(shouldKeepOutName)
 			.subscribe(true, PubSubOption.keepDuplicates(true));
-		topLeftSubscriber =
+		topLeftXSubscriber =
 			inputTable
-			.getDoubleArrayTopic(keepOutTopLeftName)
-			.subscribe(new double[] {}, PubSubOption.keepDuplicates(true));
-		bottomRightSubscriber =
+			.getDoubleTopic(keepOutTopLeftName + "X")
+			.subscribe(0.0, PubSubOption.keepDuplicates(true));
+		topLeftYSubscriber =
 			inputTable
-			.getDoubleArrayTopic(keepOutBottomRightName)
-			.subscribe(new double[] {}, PubSubOption.keepDuplicates(true));
+			.getDoubleTopic(keepOutTopLeftName + "Y")
+			.subscribe(0.0, PubSubOption.keepDuplicates(true));
+		bottomRightXSubscriber =
+			inputTable
+			.getDoubleTopic(keepOutBottomRightName + "X")
+			.subscribe(0.0, PubSubOption.keepDuplicates(true));
+		bottomRightYSubscriber =
+			inputTable
+			.getDoubleTopic(keepOutBottomRightName + "Y")
+			.subscribe(0.0, PubSubOption.keepDuplicates(true));
+		neutralZoneSubscriber =
+			inputTable
+			.getBooleanTopic(shouldNeutralZoneName)
+			.subscribe(false, PubSubOption.keepDuplicates(true));
+		avoidBumpSubscriber =
+			inputTable
+			.getBooleanTopic(shouldAvoidBumpName)
+			.subscribe(false, PubSubOption.keepDuplicates(true));
+		notAutoIntakeSubscriber =
+			inputTable
+			.getBooleanTopic(shouldNotAutoIntakeName)
+			.subscribe(false, PubSubOption.keepDuplicates(true));
+		stopToShootSubscriber =
+			inputTable
+			.getBooleanTopic(shouldStopToShootName)
+			.subscribe(false, PubSubOption.keepDuplicates(true));
 		climbSubscriber =
 			inputTable
 			.getBooleanTopic(shouldClimbName)
@@ -50,16 +84,34 @@ public class FreeRangeAutoSelectorIOServer implements FreeRangeAutoSelectorIO {
 	@Override
 	public void updateInputs(FreeRangeAutoSelectorIOInputs inputs) {
 		if (keepOutSubscriber.readQueue().length > 0) {
-			inputs.shouldKeepOut = (boolean) keepOutSubscriber.get();
+			inputs.shouldKeepOut = !(boolean) keepOutSubscriber.get();
 		}
-		if (topLeftSubscriber.readQueueValues().length > 0) {
-			inputs.keepOutTopLeft = topLeftSubscriber.get();
+		if (topLeftXSubscriber.readQueue().length > 0) {
+			inputs.keepOutTopLeft[0] = topLeftXSubscriber.get();
 		}
-		if (bottomRightSubscriber.readQueueValues().length > 0) {
-			inputs.keepOutTopRight = bottomRightSubscriber.get();
+		if (topLeftYSubscriber.readQueue().length > 0) {
+			inputs.keepOutTopLeft[1] = topLeftYSubscriber.get();
+		}
+		if (bottomRightXSubscriber.readQueue().length > 0) {
+			inputs.keepOutBottomRight[0] = bottomRightXSubscriber.get();
+		}
+		if (bottomRightYSubscriber.readQueue().length > 0) {
+			inputs.keepOutBottomRight[1] = bottomRightYSubscriber.get();
 		}
 		if (climbSubscriber.readQueue().length > 0) {
 			inputs.shouldClimb = (boolean) climbSubscriber.get();
+		}
+		if (neutralZoneSubscriber.readQueue().length > 0) {
+			inputs.shouldNeutralZone = (boolean) neutralZoneSubscriber.get();
+		}
+		if (avoidBumpSubscriber.readQueue().length > 0) {
+			inputs.shouldAvoidBump = (boolean) avoidBumpSubscriber.get();
+		}
+		if (notAutoIntakeSubscriber.readQueue().length > 0) {
+			inputs.shouldNotAutoIntake = (boolean) notAutoIntakeSubscriber.get();
+		}
+		if (stopToShootSubscriber.readQueue().length > 0) {
+			inputs.shouldStopToShoot = (boolean) stopToShootSubscriber.get();
 		}
 	}
 }
