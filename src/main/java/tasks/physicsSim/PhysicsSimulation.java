11b567186b031001776c10c35e12002807dd725c
Ladle <77344665+The-Ladle@users.noreply.github.com>
2026-03-07 16:30:47 -0500

    Correct physics?

--- src/main/java/tasks/physicsSim/PhysicsSimulation.java
+++ src/main/java/tasks/physicsSim/PhysicsSimulation.java
@@ -21,7 +21,7 @@ import frc.robot.subsystems.shooter.flywheel.FlywheelConstants;
 import frc.robot.subsystems.shooter.hood.HoodConstants;
 
 public class PhysicsSimulation {
-	private static final boolean SHOULD_SIM_DURING_BUILD = false;
+	private static final boolean SHOULD_SIM_DURING_BUILD = true;
 	public static void main(String[] args) throws InterruptedException, IOException {
 		boolean isWindows = System.getProperty("os.name").toLowerCase().contains("win");
 		boolean isLinux = System.getProperty("os.name").toLowerCase().contains("lin");
