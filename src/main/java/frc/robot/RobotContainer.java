0be46ce3bf0a4b4494e028c0a7f0152bcac07227
Michael Sims <michael.sims@bovineintervention.org>
2026-03-14 09:19:35 -0400

    add preloads

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -37,6 +37,7 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.auto.AutoManager;
 import frc.robot.auto.AutoSelector;
 import frc.robot.auto.routines.DoubleSwipe;
+import frc.robot.auto.routines.Preloads;
 import frc.robot.automations.AutoFeed;
 import frc.robot.automations.HubShiftNotifications;
 import frc.robot.automations.IntakeDeployHysteresis;
@@ -382,6 +383,7 @@ public class RobotContainer {
 		final var autoSelector = new AutoSelector("Auto Selector");
 		// Add autonomous routines to autonomous selector
 		autoSelector.addDefaultRoutine(new DoubleSwipe(this));
+		autoSelector.addRoutine(new Preloads(this));
 
 		this.autoManager = new AutoManager(autoSelector);
 
