a607ad0ba8038bf4c52ffc4845ea3b23f062a398
Michael Sims <michael.sims@bovineintervention.org>
2026-03-17 21:36:35 -0400

    Update at 'Sat Mar 14 11:54:11 EDT 2026'

--- src/main/java/frc/robot/auto/AutoConstants.java
+++ src/main/java/frc/robot/auto/AutoConstants.java
@@ -1,5 +1,6 @@
 package frc.robot.auto;
 
+import static edu.wpi.first.units.Units.Inches;
 import static edu.wpi.first.units.Units.Seconds;
 
 import edu.wpi.first.math.geometry.Pose2d;
@@ -7,6 +8,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.units.measure.Time;
 import frc.robot.constants.FieldConstants;
+import frc.robot.constants.RobotConstants;
 import frc.util.flipping.AllianceFlipped;
 
 public final class AutoConstants {
@@ -18,7 +20,8 @@ public final class AutoConstants {
 	public static final AllianceFlipped<Pose2d> startInLeftTrench = AllianceFlipped.fromBlue(
 		new Pose2d(
 			new Translation2d(
-
+				FieldConstants.robotStartingLineCenterX.getBlue().plus(RobotConstants.centerToFrontBumper),
+				FieldConstants.fieldWidth.minus(Inches.of(24.0))
 			),
 			Rotation2d.k180deg
 		)
@@ -27,7 +30,8 @@ public final class AutoConstants {
 	public static final AllianceFlipped<Pose2d> startInRightTrench = AllianceFlipped.fromBlue(
 		new Pose2d(
 			new Translation2d(
-
+				FieldConstants.robotStartingLineCenterX.getBlue().plus(RobotConstants.centerToFrontBumper),
+				Inches.of(24.0)
 			),
 			Rotation2d.k180deg
 		)
