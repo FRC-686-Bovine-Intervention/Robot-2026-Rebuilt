15dab01a40585f7639bb2b52e89b39951adbacc4
Michael Sims <michael.sims@bovineintervention.org>
2026-03-24 19:32:51 -0400

    remove tigerbots flywheel mul

--- src/main/java/frc/robot/subsystems/shooter/aiming/shooting/InterpolationShootingCalc.java
+++ src/main/java/frc/robot/subsystems/shooter/aiming/shooting/InterpolationShootingCalc.java
@@ -34,8 +34,6 @@ public class InterpolationShootingCalc implements ShootingCalc {
 		ShooterConstants.hubTargetFlyWheelVeloMPS.get(0.0);
 	}
 
-	private static final LoggedTunableNumber flywheelMul = LoggedTunable.from("FLYWHEEL MUL", 0.5);
-
 	@Override
 	public void calculate(Pose2d robotPose, ChassisSpeeds fieldSpeeds, Translation3d aimPoint) {
 		this.shotPose = robotPose.getTranslation();
@@ -54,7 +52,7 @@ public class InterpolationShootingCalc implements ShootingCalc {
 		Logger.recordOutput("Subsystems/Shooter/Aiming/Effective Distance", this.effectiveDistanceMeters);
 
 		this.targetHoodAngleRads = ShooterConstants.hubTargetHoodAngleRads.get(this.effectiveDistanceMeters);
-		this.targetFlywheelVeloMPS = ShooterConstants.hubTargetFlyWheelVeloMPS.get(this.effectiveDistanceMeters) * flywheelMul.getAsDouble();
+		this.targetFlywheelVeloMPS = ShooterConstants.hubTargetFlyWheelVeloMPS.get(this.effectiveDistanceMeters);
 		this.tofSecs = ShooterConstants.hubTargetTimeOfFlightSecs.get(this.effectiveDistanceMeters);
 
 		Logger.recordOutput("Subsystems/Shooter/Aiming/Aim Point", this.aimPoint);
